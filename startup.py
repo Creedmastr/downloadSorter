e170349ac850322a6931f98cce7828c87103a2b4
Creedmastr <63053600+Creedmastr@users.noreply.github.com>
2022-06-21 19:50:26 +0200

    The same commit I did, but the other file

--- startup.py
+++ startup.py
@@ -1,6 +1,7 @@
 def startup():
     from tkinter import simpledialog
     from os.path import exists
+    import os
 
     #First setup using Tkinter
     files_json_startup = exists("./config.json")
@@ -9,7 +10,8 @@ def startup():
     USER_CONFIG = ""
 
     if not files_json_startup:
-        USER_CONFIG = simpledialog.askstring(title="Base config starts", prompt="Please enter the path to 'C:/Users/<yourusernamehere>' (Replace the \ by /, otherwise it won't work)")
+        e = os.path.expanduser("~")
+        USER_CONFIG = e.replace("\\", "/")
         if USER_CONFIG == "":
             exit()
         USER_CONFIG_TOWRITE = '{ "user_path_input": "' + str(USER_CONFIG) + '" }'
